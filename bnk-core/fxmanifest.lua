71bc1e773462898415129564effd702053ad7121
BNK GOD <kinsleyalfonso2022@gmail.com>
2024-06-09 19:56:36 -0700

    update

---
+++ bnk-core/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'bnk.god'
+description 'bnk-core'
+version '1.0.0'
+
+lua54 'yes'
+
+shared_script {
+    'config.lua'
+}
+
+
+client_script {
+    'client/*.lua'
+}
+
+server_script {
+    'server/*.lua'
+}
